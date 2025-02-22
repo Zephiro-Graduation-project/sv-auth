2a52e7e41f2684282b5dbdc580c7973fb7d5c563
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2025-02-21 21:39:51 -0500

    Registro funcional

--- src/main/java/com/zephiro/auth/controller/UserController.java
+++ src/main/java/com/zephiro/auth/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zephiro.auth.DTO.UserDTO;
+import com.zephiro.auth.model.UserEntity;
 import com.zephiro.auth.security.JWTGenerator;
 import com.zephiro.auth.service.UserService;
 
@@ -30,6 +32,10 @@ public class UserController {
     @Autowired
     private JWTGenerator jwtGenerator;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // ToDo: Manejar la encriptación de la contraseña desde el front
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDTO user) {
         try {
@@ -54,6 +60,20 @@ public class UserController {
             // Manejar otros errores
             System.out.println("Error:\n" + e);
             return new ResponseEntity<>("Error occurred during login", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // ToDo: Manejar la encriptación de la contraseña desde el front
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserEntity user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userService.save(user);
+            return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            // Manejar errores
+            System.out.println("Error:\n" + e);
+            return new ResponseEntity<>("Error occurred during registration", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
