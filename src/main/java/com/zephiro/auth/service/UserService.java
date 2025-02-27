33998f8cc500246529f5221d51d1bc4e80779d7e
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2025-02-26 23:17:26 -0500

    Desencriptar la contraseña recibida

--- src/main/java/com/zephiro/auth/service/UserService.java
+++ src/main/java/com/zephiro/auth/service/UserService.java
@@ -1,6 +1,12 @@
 package com.zephiro.auth.service;
 
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,11 +36,15 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    // ToDo: Manejar la encriptación de la contraseña desde el front
+    @Value("${SECRET_KEY}")
+    private String secretKey;
+
     public Account login(UserDTO user) {
         try {
+            String decryptedPassword = decryptPassword(user.getPassword());
+
             Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getMail(), user.getPassword()));
+                    new UsernamePasswordAuthenticationToken(user.getMail(), decryptedPassword));
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
@@ -50,13 +60,26 @@ public class UserService {
         }
     }
 
-    // ToDo: Manejar la encriptación de la contraseña desde el front
     public void register(UserEntity user) {
         try {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            String decryptedPassword = decryptPassword(user.getPassword());
+            user.setPassword(passwordEncoder.encode(decryptedPassword));
             userRepository.save(user);
         } catch (Exception e) {
             throw new RuntimeException("Error occurred during registration", e);
         }
     }
+
+    private String decryptPassword(String encryptedPassword) {
+    try {
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedPassword);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted);
+    } catch (Exception e) {
+        throw new RuntimeException("Error decrypting password", e);
+    }
+}
 }
